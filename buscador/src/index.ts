01f0171ef8317e02c8949ecc8be7362b7979f0c0
werton <mr.werton@gmail.com>
2023-03-05 14:43:24 -0300

    feat: refactor

--- buscador/src/index.ts
+++ buscador/src/index.ts
@@ -11,83 +11,55 @@ const prisma = new PrismaClient()
 app.post('/', async (req, res) => {
     const { url, term, deep } = req.body;
 
+    const storage = await prisma.storage.findFirst({
+        where: {
+            url: url, term: term, deep: deep
+        },
+        include: {
+            pages: true
+        }
+    })
 
-    /*    const storage = await prisma.storage.findFirst({
-           where: {
-               url: url, term: term, deep: deep
-           },
-           include: {
-               pages: true
-           }
-       })
-   
-       if (storage) {
-           return res.json({ storage })
-   
-       } */
-    const guugre = new Guugri();
+    if (storage) {
+        return res.json({ storage })
+
+    }
 
     const browser = await puppeteer.launch({
         headless: true
     });
     const page = await browser.newPage();
-    await guugre.search(url, term, deep, browser, page);
+
+    const guugre = new Guugri(page);
+
+    await guugre.search(url, term, deep);
+
+    const pageWithScore = guugre.getPageWithScore()
+
+    await prisma.storage.create({
+        data: {
+            deep: deep,
+            url: url,
+            term: term,
+            pages: {
+                create: pageWithScore.map(item => {
+                    return {
+                        score: item.score,
+                        url: url,
+                        content: item.content ?? ''
 
 
-    const occursOfLinks = guugre.pages.map((item: any) => {
-        const reference_amount = guugre.allLinks.filter(link => item.link === link).length;
-        return {
-            reference_amount: reference_amount,
-            ...item
-        }
+                    }
+                }),
+            }
+
+        },
     })
-
-    const scorePage = occursOfLinks.map(item => {
-        const hasSemantic = item.hasSemantic ? 0 : -1
-        const hasH1 = item.hasH1 ? 2 : 0
-        return {
-            score: item.reference_amount + hasSemantic + hasH1,
-            content: item.content,
-            link: item.link,
-            ...item,
-        }
-    })
-    const order = scorePage.sort(compare)
-
-    /*  await prisma.storage.create({
-         data: {
-             deep: deep,
-             url: url,
-             term: term,
-             pages: {
-                 create: scorePage.map(item => {
-                     return {
-                         score: item.score,
-                         url: url,
-                         content: item.content ?? ''
- 
- 
-                     }
-                 }),
-             }
- 
-         },
-     }) */
     guugre.clearAll()
     browser.close();
 
-    return res.json({ search: order })
-
-
+    return res.json({ search: pageWithScore })
 
 })
-function compare(a: any, b: any) {
-    if (a.score < b.score) {
-        return 1;
-    }
-    if (a.score > b.score) {
-        return -1;
-    }
-    return 0;
-}
+
 app.listen(4000, () => (`server run in http://localhost:4000`))
--- buscador/src/services/guugri.ts
+++ buscador/src/services/guugri.ts
@@ -1,28 +1,41 @@
-import { Browser, Page } from "puppeteer";
+import { Page } from "puppeteer";
 
+export interface IPageProps {
+    link: string,
+    content: string,
+    hasH1: boolean,
+    hasSemantic: boolean,
+
+}
 export class Guugri {
     private linksCached = new Set();
-    public allLinks: string[] = [];
-    public pages: Object[] = [];
+    private allLinks: string[] = [];
+    public pages: IPageProps[] = [];
+    private _page: Page;
+    constructor(
+        page: Page
+    ) {
+        this._page = page;
+    }
 
-    async search(url: string, term: string, depth: number, browser: Browser, page: Page) {
+    async search(url: string, term: string, depth: number) {
         this.linksCached.add(url)
 
         console.log(`searching in ${url}`)
 
-        await page.goto(url, { waitUntil: 'networkidle2', timeout: 0 });
-        page.setDefaultNavigationTimeout(0)
+        await this._page.goto(url, { waitUntil: 'networkidle2', timeout: 0 });
+        this._page.setDefaultNavigationTimeout(0)
 
-        const hasH1 = await this.verifyIfPageHasH1(page);
-        const termInPage = await this.getTermInPage(page, term);
-        const hasSemantic = await this.verifyIfPageHasSemantic(page);
+        const termInPage = await this._getTermInPage(this._page, term);
+        const hasH1 = await this._verifyIfPageHasH1(this._page);
+        const hasSemantic = await this._verifyIfPageHasSemantic(this._page);
 
         if (termInPage !== null) {
             this.pages.push({ link: url, content: termInPage, hasH1, hasSemantic })
         }
 
         if (depth !== 0) {
-            const links = await this.getAllLinksOfPage(page, url)
+            const links = await this._getAllLinksOfPage(this._page, url)
             const filteredLinks = links.filter((link) => link !== url)
             this.allLinks.push(...filteredLinks)
             for (let i = 0; i < links.length; i++) {
@@ -30,7 +43,7 @@ export class Guugri {
 
                 if (!this.linksCached.has(currentLink)) {
                     try {
-                        await this.search(currentLink, term, depth - 1, browser, page);
+                        await this.search(currentLink, term, depth - 1);
                     } catch (err) {
                         return
                     }
@@ -40,38 +53,33 @@ export class Guugri {
 
         }
     }
-    private async getAllLinksOfPage(page: Page, url: string) {
-        const links = await page.$$eval('a', (anchors) => {
-            return anchors.map((a) => a.href);
-        });
 
-        const regexDomain = /^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n]+)/i;
 
-        const domain = url?.match(regexDomain)![1];
-        const linksLessCurrent = links.filter(link => {
-            return link.includes(domain)
-        });
-
-        return linksLessCurrent;
-    }
-
-    private async getTermInPage(page: Page, term: string) {
+    private async _getTermInPage(page: Page, term: string) {
 
         const contentPage = await page.content();
         const regexForRemoveTags = /(<([^>]+)>)/ig;
         const contentWithoutTag = contentPage.replace(regexForRemoveTags, '');
         const content = contentWithoutTag?.replace(/^\s+/gm, '')?.match(new RegExp(`.{0,20}\\b${term}\\b.{0,20}`, 'gim'));
 
-        const removedNull = content?.filter(item => item !== null).join(" ----> ");
+        const contentRemovedNull = content?.filter(item => item !== null).join(" ----> ");
 
-        if (removedNull?.length === undefined || removedNull.trim().length === 0) {
+        if (contentRemovedNull?.length === undefined || contentRemovedNull.trim().length === 0) {
             return null;
         }
 
-        return removedNull
+        return contentRemovedNull
 
     }
-    private async verifyIfPageHasSemantic(page: Page) {
+    private async _verifyIfPageHasH1(page: Page) {
+        const hasH1 = await page.evaluate(() => {
+            return document.querySelector('h1') !== null;
+        });
+
+        return hasH1;
+
+    }
+    private async _verifyIfPageHasSemantic(page: Page) {
         const hasHeader = await page.evaluate(() => {
             return document.querySelector('header') !== null;
         });
@@ -88,14 +96,63 @@ export class Guugri {
             return false;
         }
     }
-    private async verifyIfPageHasH1(page: Page) {
-        const hasH1 = await page.evaluate(() => {
-            return document.querySelector('h1') !== null;
+
+    private async _getAllLinksOfPage(page: Page, url: string) {
+        const links = await page.$$eval('a', (link) => {
+            return link.map((a) => a.href);
         });
 
-        return hasH1;
+        const regexDomain = /^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n]+)/i;
 
+        const domain = url?.match(regexDomain)![1];
+
+        /*  const removeLinkExternal = links.filter(link => {
+             return link.includes('facebook') || link.includes("google") || link.includes("twitter")
+         }); */
+
+        const linksOnlyOfDomain = links.filter(link => {
+            return link.includes(domain)
+        });
+
+        return linksOnlyOfDomain;
     }
+    private _getOccursOfLinks() {
+        const occursOfLinks = this.pages.map((item: any) => {
+            const reference_amount = this.allLinks.filter(link => item.link === link).length;
+            return {
+                reference_amount: reference_amount,
+                ...item
+            }
+        })
+        return occursOfLinks;
+    }
+
+    getPageWithScore() {
+        const occursOfLinks = this._getOccursOfLinks();
+        const scorePage = occursOfLinks.map(item => {
+            const hasSemantic = item.hasSemantic ? 0 : -1
+            const hasH1 = item.hasH1 ? 2 : 0
+            return {
+                score: item.reference_amount + hasSemantic + hasH1,
+                content: item.content,
+                link: item.link,
+
+                ...item,
+            }
+        })
+        return scorePage.sort(this._compare)
+    }
+
+    private _compare(a: any, b: any) {
+        if (a.score < b.score) {
+            return 1;
+        }
+        if (a.score > b.score) {
+            return -1;
+        }
+        return 0;
+    }
+
     clearAll() {
         this.allLinks = []
         this.pages = []
